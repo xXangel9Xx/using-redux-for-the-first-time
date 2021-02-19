51412907749b6711bf263f758e8de59db7f456e7
xXangelXx <angeluchimag17@gmail.com>
2021-02-19 16:18:15 -0400

    create components main

---
+++ src/components/results/index.jsx
@@ -0,0 +1,9 @@
+import React,{useState,useEffect} from 'react'
+const Index = () =>{
+    return (
+        <div>
+
+        </div>
+    );
+};
+export default Index
---
+++ src/pages/Pageresults/results.jsx
@@ -0,0 +1,9 @@
+import React, {useState,useEffect} from 'react'
+const Results = () =>{
+    return (
+        <div className="container-page-results">
+            
+        </div>
+    )
+}
+export default Results
