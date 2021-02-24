0037a5ba743eb194ab52c1b4ddc646f8474b6dfe
xXangelXx <angeluchimag17@gmail.com>
2021-02-24 14:46:40 -0400

    redux/store.js import reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,9 @@ import currentItem from './reducers/currentItem';
 import results from './reducers/results';
 import suggestions from './reducers/suggestions';
 const reducer = combineReducers[{
-
+    currentItem,
+    results,
+    suggestions
 }];
 const store = createStore(reducer);
 export default store
