1e68fd23adc3008becd46f2a9964cb10cc31b4e1
xXangelXx <angeluchimag17@gmail.com>
2021-02-24 15:53:00 -0400

    comment to page results

--- src/pages/pageResults/results.jsx
+++ src/pages/pageResults/results.jsx
@@ -1,12 +1,25 @@
 import React, {useState,useEffect} from 'react'
+import { connect } from 'react-redux';
 import Index from '../../components/results/index' 
 const Results = () =>{
     return (
         <div className="container-page-results">
-            <Index 
-            
-            />
+            <Index/>
         </div>
     )
 }
-export default Results
+
+const mapStateToProps = ( state ) =>{
+    return {
+        suggestions: state.suggestions
+    }
+};/*
+//////////////////////ESTO////////////////////
+const wrapper = connect(mapStateToProps)
+const component = wrapper(Results); //wrapper tiene que saber que componente va a modifical
+export default component 
+/////////////////////Y esto//////////////////
+export default connect(mapStateToProps)(Results)
+/////////////////////Es igual////////////////
+*/
+export default connect(mapStateToProps)(Results)
