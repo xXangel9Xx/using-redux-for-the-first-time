593158b672ee7afe2254218a7e5c5baa26a5498b
xXangelXx <angeluchimag17@gmail.com>
2021-02-24 14:37:00 -0400

    import in store.js redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
 import { createStore,combineReducers } from 'redux';
+import currentItem from './reducers/currentItem';
+import results from './reducers/results';
+import suggestions from './reducers/suggestions';
 const reducer = combineReducers[{
 
 }];
